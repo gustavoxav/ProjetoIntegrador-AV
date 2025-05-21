0cc874ec745917eca7d38c0b03ff14949479278e
thiagorochatr <thiagord2016@gmail.com>
2025-05-20 23:13:51 -0300

    refactor arquivos de front relacionados ao cliente

--- front/src/controladora/controladora-cliente.ts
+++ front/src/controladora/controladora-cliente.ts
@@ -1,6 +1,6 @@
 import { ErroDominio } from "../infra/ErroDominio.js";
 import { GestorCliente } from "../gestor/gestor-cliente.js";
-import { VisaoCliente } from "../visao/visao-clientes.js";
+import type { VisaoCliente } from "../visao/visao-clientes.js";
 
 export class ControladoraCliente {
   constructor(private visao: VisaoCliente) {}
@@ -9,13 +9,15 @@ export class ControladoraCliente {
     const visaoFilter = this.visao.filtroClientes();
     const gestor = new GestorCliente();
     try {
-      const response = await gestor.obterClientes(parseInt(visaoFilter.filtro));
+      const response = await gestor.obterClientes(Number.parseInt(visaoFilter.filtro));
       this.visao.retornarClientes(response);
-    } catch (error: any) {
+    } catch (error: unknown) {
       if (error instanceof ErroDominio) {
         this.visao.exibirMensagens(error.getProblemas());
-      } else {
+      } else if (error instanceof Error) {
         this.visao.exibirMensagens([error.message]);
+      } else {
+        this.visao.exibirMensagens(["Erro desconhecido"]);
       }
     }
   }
