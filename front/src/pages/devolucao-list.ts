cb4db2602f0fa67d2aad25a1e37c63b27ee521ad
thiagorochatr <thiagord2016@gmail.com>
2025-05-23 19:52:11 -0300

    fix: remove botão Adicionar na listagem de devolução

--- front/src/pages/devolucao-list.ts
+++ front/src/pages/devolucao-list.ts
@@ -1,17 +1,5 @@
 import { VisaoDevolucaoEmHTML } from "../visao/visao-devolucao-html";
 
 export function initDevolucaoList() {
-  const addButton = document.getElementById("addButton");
   new VisaoDevolucaoEmHTML();
-
-  if (addButton) {
-    addButton.addEventListener("click", () => {
-      window.history.pushState({}, "", "/devolucao-add");
-      window.dispatchEvent(
-        new CustomEvent("routeChange", {
-          detail: { path: "/devolucao-add" },
-        })
-      );
-    });
-  }
 }
